1606d2734f288dc2d8f90c39f4a6a92cb2d89684
Nick <69139909+NickFDI@users.noreply.github.com>
2025-03-15 10:08:43 -0400

    DNS update

--- networking/kaliinitial.sh
+++ networking/kaliinitial.sh
@@ -6,6 +6,7 @@ INTERFACE="eth0"
 DEFAULT_IP="192.168.16.4"
 DEFAULT_NETMASK="255.255.255.0"
 DEFAULT_GATEWAY="192.168.16.1"
+DEFAULT_DNS="ns1.team16.net"
 
 # Check the networking interface
 if ! ip link show | grep -q "$INTERFACE"; then
@@ -26,8 +27,13 @@ ip route add default via $DEFAULT_GATEWAY
 
 ip link set $INTERFACE up
 
+echo "nameserver $DEFAULT_DNS" | sudo tee /etc/resolv.conf > /dev/null
+
 if ip addr show $INTERFACE | grep -q "state UP"; then
     echo "$INTERFACE is now up and running."
+    echo "DNS server set to $DEFAULT_DNS."
+    echo "Gateway set to $DEFAULT_GATEWAY."
+
 else
     echo "Failed to bring up $INTERFACE."
 fi
